57879a74316f1476e433802bba151e6c300de06e
PashaShevelev <62694731+PashaShevelev@users.noreply.github.com>
2020-06-27 09:03:20 +0500

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -4,17 +4,16 @@ from get_args import pars_arg
 from functools import partial
 
 
-def port_is_open(port, state):
-    """Анализировать будем только на локальной машине, по понятным причинам"""
+def port_is_open(port, ip, state):
     sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    con_tcp, con_udp = sock_tcp.connect_ex(('localhost', port)), sock_udp.connect_ex(('localhost', port))
+    con_tcp, con_udp = sock_tcp.connect_ex((ip, port)), sock_udp.connect_ex((ip, port))
 
     try:
         if not con_tcp and state[0]:
-            print(f"TCP {port} {socket.getservbyport(port)}")
+            print(f"TCP {port} {get_protocol(socket.getservbyport(port))}")
         if not con_udp and state[1]:
-            print(f"UDP {port} {socket.getservbyport(port)}")
+            print(f"UDP {port}")
     except OSError:
         if not con_tcp and state[0]:
             print(f"TCP {port}")
@@ -25,30 +24,46 @@ def port_is_open(port, state):
         sock_udp.close()
 
 
-def port_scanner(left, right, state):
+def get_protocol(data):
+    if data == 'domain':
+        return 'DNS'
+    elif data == 'http':
+        return 'HTTP'
+    elif data == 'pop3':
+        return 'POP3'
+    elif data == 'ntp':
+        return 'NTP'
+    elif data == 'imap':
+        return 'IMAP'
+    elif data == 'smtp':
+        return 'SMTP'
+    else:
+        return ''
+
+
+def port_scanner(left, right, ip, state):
     for port in range(left, right):
-        port_is_open(port, state)
+        port_is_open(port, ip, state)
 
 
 def main():
     data = pars_arg()
-    state = data[:2]
-    left, right = data[2]
+    ip = data[0]
+    state = data[1:3]
+    left, right = data[3]
     lo, hi = 0, 65_355
     if lo <= left and right <= hi:
         if left - right > 100:
             with futures.ThreadPoolExecutor(max_workers=4) as executor:
                 spawn = partial(executor.submit, port_scanner)
-                fs = [spawn(bord_l, bord_l + 100, state) for bord_l in range(left, right + 1, 100)]
+                fs = [spawn(bord_l, bord_l + 100, ip, state) for bord_l in range(left, right + 1, 100)]
                 for i in futures.as_completed(fs):
                     i.result()
         else:
-            port_scanner(left, right + 1, state)
+            port_scanner(left, right + 1, ip, state)
     else:
         print(f'Range [{left}, {right}] is incorrect.')
 
 
 if __name__ == '__main__':
     main()
-
-
