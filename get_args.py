c7b575a930cb5030396c29c7885e451e956fae12
PashaShevelev <62694731+PashaShevelev@users.noreply.github.com>
2020-06-27 09:01:55 +0500

    Update get_args.py

--- get_args.py
+++ get_args.py
@@ -3,8 +3,9 @@ import argparse
 
 def pars_arg():
     pars = argparse.ArgumentParser()
+    pars.add_argument('-ip', default='localhost')
     pars.add_argument('-t', action="store_true")
     pars.add_argument('-u', action="store_true")
     pars.add_argument('-p', '--ports', nargs=2, type=int)
     args = pars.parse_args()
-    return args.t, args.u, args.ports
+    return args.ip, args.t, args.u, args.ports
