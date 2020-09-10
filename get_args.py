72f2f1adca2a5614667adab5543709c0639083d0
PashaShevelev <62694731+PashaShevelev@users.noreply.github.com>
2020-09-11 04:21:39 +0500

    Update get_args.py

--- get_args.py
+++ get_args.py
@@ -6,6 +6,8 @@ def pars_arg():
     pars.add_argument('-ip', default='localhost')
     pars.add_argument('-t', action="store_true")
     pars.add_argument('-u', action="store_true")
+    pars.add_argument('-w', default=4, help='max count workers')
     pars.add_argument('-p', '--ports', nargs=2, type=int)
+    
     args = pars.parse_args()
-    return args.ip, args.t, args.u, args.ports
+    return args.ip, args.t, args.u, args.ports, args.w
